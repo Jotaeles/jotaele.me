6680a55b40a8deea33179dba3263b6636aa2aaa0
jotaeles <jl_sandoval_salas@hotmail.com>
2021-03-18 22:48:30 -0500

    Add Cover component

---
+++ components/modules/Cover.js
@@ -0,0 +1,9 @@
+export default function Cover() {
+  return (
+    <div className="h-96 md:h-screen max-w-7xl mx-auto flex items-end">
+      <h1 className="font-bold text-3xl md:text-7xl lg:text-8xl text-primary max-w-xs md:max-w-full lg:max-w-4xl dark:text-gray-200 md:mb-20 p-7 lg:p-0">
+        I&apos;m Frontend Developer based in Saltillo, México.
+      </h1>
+    </div>
+  );
+}
