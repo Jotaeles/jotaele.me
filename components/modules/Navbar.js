d59cc80fbe36b03d54fd04e53c671c5a458a4f3a
jotaeles <jl_sandoval_salas@hotmail.com>
2021-03-18 22:48:15 -0500

    fix: Navbar component

--- components/modules/Navbar.js
+++ components/modules/Navbar.js
@@ -19,45 +19,47 @@ export default function Navbar() {
   }, [theme]);
 
   return (
-    <nav className="sticky-nav w-full p-8 flex justify-between items-center bg-white bg-opacity-60 dark:bg-primary">
-      <Link href="/">
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a className="block">
-          <img className="h-8" src={urlImage} alt="Jotaele Logo" />
-        </a>
-      </Link>
-      <button
-        aria-label="Toggle Dark Mode"
-        type="button"
-        className="bg-gray-100 dark:bg-gray-700 rounded p-3 h-10 w-10 focus:outline-none"
-        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-      >
-        {mounted && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            stroke="currentColor"
-            className="h-4 w-4 text-primary dark:text-gray-200"
-          >
-            {theme === 'dark' ? (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            ) : (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-              />
-            )}
-          </svg>
-        )}
-      </button>
+    <nav className="fixed-nav w-full p-7 bg-white bg-opacity-60 dark:bg-primary">
+      <div className="flex justify-between items-center max-w-7xl mx-auto">
+        <Link href="/">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a className="block">
+            <img className="h-8" src={urlImage} alt="Jotaele Logo" />
+          </a>
+        </Link>
+        <button
+          aria-label="Toggle Dark Mode"
+          type="button"
+          className="bg-gray-100 dark:bg-gray-700 rounded p-3 h-10 w-10 focus:outline-none"
+          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        >
+          {mounted && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="currentColor"
+              className="h-4 w-4 text-primary dark:text-gray-200"
+            >
+              {theme === 'dark' ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              )}
+            </svg>
+          )}
+        </button>
+      </div>
     </nav>
   );
 }
